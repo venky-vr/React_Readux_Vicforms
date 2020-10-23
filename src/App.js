cee02a01561e609c99366dc61066478328c92602
venkyvr <vboyini1@gmail.com>
2020-10-23 13:56:09 +0530

    search-filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
 import { Redirect, Route, Switch } from "react-router-dom";
@@ -7,12 +7,15 @@ import Home from "./components/Home/Home";
 import Overview from "./components/Overview/Overview";
 import Contracts from "./components/Contracts/Contracts";
 import Snippets from "./components/Snippets/Snippets"; 
+import SearchFilter from "./components/SearchFilter/SearchFilter";
 
 
-function App() {
+class App extends Component {
+  render(){
   return (
-    <div className="App">
+    <div className="">
        <Navbar/>
+       <SearchFilter items={this.props.products}/>
        <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/overview" component={Overview} />
@@ -22,6 +25,7 @@ function App() {
         </Switch> 
     </div>
   );
+  }
 }
 
 export default App;
